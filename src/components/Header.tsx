4fa3ec2affe3fc6e3f0505906f5de5ee16a75750
big-bunny <codecenter000@gmail.com>
2023-06-27 22:52:28 +0300

    fucker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,194 +30,185 @@ const Header: React.FC = () => {
 
   return (
     <div>
-    <header className="bg-gray-600 rounded-3xl">
-      <nav className="container mx-auto">
-        <div className="flex items-center justify-between p-4">
-          <Link href="/" legacyBehavior>
-            <a className="text-white text-3xl font-bold">Schield Centre</a>
-          </Link>
-          {/* Hamburger menu */}
-          <div className="md:hidden">
-            <button
-              type="button"
-              className="text-white hover:text-green-300 focus:outline-none"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+      <header className="bg-gray-600 rounded-3xl">
+        <nav className="container mx-auto">
+          <div className="flex items-center justify-between p-4">
+            <Link href="/" legacyBehavior>
+              Schield Centre
+            </Link>
+            {/* Hamburger menu */}
+            <div className="md:hidden">
+              <button
+                type="button"
+                className="text-white hover:text-green-300 focus:outline-none"
+                onClick={toggleMenu}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
-        </div>
-        {/* Mobile menu */}
-        {isMenuOpen && (
-          <ul className="md:hidden bg-gray-900 text-white px-4 py-2">
+          {/* Mobile menu */}
+          {isMenuOpen && (
+            <ul className="md:hidden bg-gray-900 text-white px-4 py-2">
+              {menuLinks.map((link) => (
+                <li key={link.href}>
+                  {link.label}
+                </li>
+              ))}
+              <li>
+                <button onClick={toggleDropdown} className="block py-2">
+                  Sponsor
+                </button>
+                {isDropdownOpen && (
+                  <ul className="ml-4 bg-white text-gray-900 rounded-md shadow-lg py-2">
+                    <li>
+                      <Link href="/child" className="block py-2 hover:text-blue-500">
+                        Child
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/program" className="block py-2 hover:text-blue-500">
+                        Program
+                      </Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li>
+                {!session && (
+                  <>
+                    <span className="mr-2">You are not signed in</span>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        signIn();
+                      }}
+                      className="text-white hover:text-green-300"
+                    >
+                      Sign in
+                    </button>
+                  </>
+                )}
+                {session?.user && (
+                  <>
+                    <div
+                      className="w-8 h-8 rounded-full bg-cover bg-center mr-2"
+                      style={{ backgroundImage: `url(${session.user.image})` }}
+                    />
+                    <div>
+                      <small className="block">Signed in as</small>
+                      <strong className="block text-white">
+                        {session.user.email || session.user.name}
+                      </strong>
+                    </div>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        signOut();
+                      }}
+                      className="text-white hover:text-green-300 ml-4"
+                    >
+                      Sign out
+                    </button>
+                  </>
+                )}
+              </li>
+              <li className="mt-4"></li>
+            </ul>
+          )}
+          {/* Desktop menu */}
+          <ul className="hidden md:flex items-center justify-between p-4">
             {menuLinks.map((link) => (
               <li key={link.href}>
-                <a href={link.href} className="block text-xl font-bold py-2">
+                <Link href={link.href} className="text-black font-bold text-xl">
                   {link.label}
-                </a>
+                </Link>
               </li>
             ))}
-            <li>
-              <a href="#" onClick={toggleDropdown} className="block py-2">
+            <li className="relative">
+              <button onClick={toggleDropdown} className="text-black hover:text-green-300">
                 Sponsor
-              </a>
+              </button>
               {isDropdownOpen && (
-                <ul className="ml-4 bg-white text-gray-900 rounded-md shadow-lg py-2">
+                <ul className="absolute top-10 right-0 bg-white text-gray-900 rounded-md shadow-lg p-2">
                   <li>
-                    <a href="/child" className="block py-2 hover:text-blue-500">
+                    <Link href="/child" className="block py-2 hover:text-blue-500">
                       Child
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a href="/program" className="block py-2 hover:text-blue-500">
+                    <Link href="/program" className="block py-2 hover:text-blue-500">
                       Program
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               )}
             </li>
             <li>
-              {!session && (
-                <>
-                  <span className="mr-2">You are not signed in</span>
-                  <a
-                    href={`/api/auth/signin`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      signIn();
-                    }}
-                    className="text-white hover:text-green-300"
-                  >
-                    Sign in
-                  </a>
-                </>
-              )}
-              {session?.user && (
-                <>
-                  <div
-                    className="w-8 h-8 rounded-full bg-cover bg-center mr-2"
-                    style={{ backgroundImage: `url(${session.user.image})` }}
-                  />
-                  <div>
-                    <small className="block">Signed in as</small>
-                    <strong className="block text-white">
-                      {session.user.email || session.user.name}
-                    </strong>
-                  </div>
-                  <a
-                    href={`/api/auth/signout`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      signOut();
-                    }}
-                    className="text-white hover:text-green-300 ml-4"
-                  >
-                    Sign out
-                  </a>
-                </>
-              )}
-            </li>
-            <li className="mt-4">
-            
+              <div className="flex items-center">
+                {!session && (
+                  <>
+                    <span className="mr-2">You are not signed in</span>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        signIn();
+                      }}
+                      className="text-black hover:text-green-300"
+                    >
+                      Sign in
+                    </button>
+                  </>
+                )}
+                {session?.user && (
+                  <>
+                    <div
+                      className="w-8 h-8 rounded-full bg-cover bg-center mr-2"
+                      style={{ backgroundImage: `url(${session.user.image})` }}
+                    />
+                    <div>
+                      <small className="block">Signed in as</small>
+                      <strong className="block text-black">
+                        {session.user.email || session.user.name}
+                      </strong>
+                    </div>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        signOut();
+                      }}
+                      className="text-black hover:text-green-300 ml-4"
+                    >
+                      Sign out
+                    </button>
+                  </>
+                )}
+              </div>
             </li>
           </ul>
-        )}
-        {/* Desktop menu */}
-        <ul className="hidden md:flex items-center justify-between p-4">
-          {menuLinks.map((link) => (
-            <li key={link.href}>
-              <a href={link.href} className="text-black font-bold text-xl">
-                {link.label}
-              </a>
-            </li>
-          ))}
-          <li className="relative">
-            <a href="#" onClick={toggleDropdown} className="text-black hover:text-green-300">
-              Sponsor
-            </a>
-            {isDropdownOpen && (
-              <ul className="absolute top-10 right-0 bg-white text-gray-900 rounded-md shadow-lg p-2">
-                <li>
-                  <a href="/child" className="block py-2 hover:text-blue-500">
-                    Child
-                  </a>
-                </li>
-                <li>
-                  <a href="/program" className="block py-2 hover:text-blue-500">
-                    Program
-                  </a>
-                </li>
-              </ul>
-            )}
-          </li>
-          <li>
-            <div className="flex items-center">
-              {!session && (
-                <>
-                  <span className="mr-2">You are not signed in</span>
-                  <a
-                    href={`/api/auth/signin`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      signIn();
-                    }}
-                    className="text-black hover:text-green-300"
-                  >
-                    Sign in
-                  </a>
-                </>
-              )}
-              {session?.user && (
-                <>
-                  <div
-                    className="w-8 h-8 rounded-full bg-cover bg-center mr-2"
-                    style={{ backgroundImage: `url(${session.user.image})` }}
-                  />
-                  <div>
-                    <small className="block">Signed in as</small>
-                    <strong className="block text-black">
-                      {session.user.email || session.user.name}
-                    </strong>
-                  </div>
-                  <a
-                    href={`/api/auth/signout`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      signOut();
-                    }}
-                    className="text-black hover:text-green-300 ml-4"
-                  >
-                    Sign out
-                  </a>
-                </>
-              )}
-            </div>
-          </li>
-        
-        </ul>
-      </nav>
-    </header>
-    <Link
-                   href="/donatepage"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  >
-                    Donate
-                  </Link>
-    </div> 
-     );
+        </nav>
+      </header>
+      <Link
+        href="/donatepage"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Donate
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
