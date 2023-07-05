099421677d8aa7231bfbf9c5aeeb1bcdd5e9110d
big-bunny <codecenter000@gmail.com>
2023-07-05 15:48:44 +0300

    vercel10

--- src/components/Donate.tsx
+++ src/components/Donate.tsx
@@ -9,26 +9,21 @@ interface WindowWithPayPal extends Window {
 }
 
 const DonateButton: React.FC = () => {
-  // State to track whether the button is rendered
   const [isButtonRendered, setIsButtonRendered] = useState(false);
 
   useEffect(() => {
-    // Check if the button is not already rendered
     if (!isButtonRendered) {
-      // Create a new script element
       const script = document.createElement('script');
       script.src = 'https://www.paypalobjects.com/donate/sdk/donate-sdk.js';
       script.charset = 'UTF-8';
       script.async = true;
 
-      // Script onload event handler
       script.onload = () => {
         if (
           (window as WindowWithPayPal).PayPal &&
           (window as WindowWithPayPal).PayPal.Donation &&
           (window as WindowWithPayPal).PayPal.Donation.Button
         ) {
-          // Render the PayPal donation button
           (window as WindowWithPayPal).PayPal.Donation.Button({
             env: 'production',
             hosted_button_id: 'WMBKQ9WVGWCXL',
@@ -41,17 +36,14 @@ const DonateButton: React.FC = () => {
         }
       };
 
-      // Append the script to the donate-button-container element
       document.getElementById('donate-button-container')?.appendChild(script);
 
-      // Set the button rendered state to true
       setIsButtonRendered(true);
     }
   }, [isButtonRendered]);
 
   return (
     <div id="donate-button-container">
-      {/* Render the donate-button div if the button is rendered */}
       {isButtonRendered ? <div id="donate-button"></div> : null}
     </div>
   );
