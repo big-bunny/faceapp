77a58489c7bb98b1271335433fdc40687bd7ce6e
big-bunny <codecenter000@gmail.com>
2023-07-05 16:42:03 +0300

    vercel16

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,41 +1,23 @@
-import NextAuth from "next-auth";
-import Auth0Provider from "next-auth/providers/auth0";
-import FacebookProvider from "next-auth/providers/facebook";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
-import { PrismaClient } from '@prisma/client';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { NextAuthOptions } from 'next-auth';
+import { getPrismaClient } from '../db';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import crypto from 'crypto';
-import { getPrismaClient } from "../db";
 
 const prisma = getPrismaClient();
 
 const options: NextAuthOptions = {
   providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      domain: process.env.AUTH0_DOMAIN,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user',
+     
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
     }),
     CredentialsProvider({
       name: 'Credentials',
@@ -43,8 +25,8 @@ const options: NextAuthOptions = {
         username: { label: 'Username', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials: { username: any; password: any }) {
-        const { username, password } = credentials;
+      async authorize(credentials) {
+        const { username, password } = credentials as { username: string; password: string };
 
         // Check if the user already exists
         const existingUser = await prisma.user.findUnique({
@@ -63,7 +45,7 @@ const options: NextAuthOptions = {
               authentication_key: authenticationKey,
               // Add any additional fields for the user
               id_: 0, // Provide a value for the id_ field
-              username: "SomeUsername", // Provide a value for the username field
+              username: 'SomeUsername', // Provide a value for the username field
             },
           });
 
@@ -84,7 +66,6 @@ const options: NextAuthOptions = {
   },
   jwt: {
     secret: process.env.NEXTAUTH_SECRET,
-    encryption: true,
   },
   pages: {
     // Custom pages...
@@ -93,11 +74,11 @@ const options: NextAuthOptions = {
   callbacks: {
     async redirect({ url, baseUrl }) {
       // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
       // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url
-      return baseUrl
-    }
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
   },
   events: {},
   debug: false,
